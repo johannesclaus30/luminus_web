f1c24d95c3268994c4982a7779d8fa472b10e3fd
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2026-03-23 16:13:37 +0800

    3/23 Commit 4PM

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -15,12 +15,21 @@
     </nav>
 
     <div class="layout-wrapper">
-        <div class="admin-dashboard-container">
+        <div class="div-dashboard-container">
             <h1>Admin Dashboard</h1>
-
         </div>
         <div class="admin-menu">
-            <h1>Admin Dashboard</h1>
+            <p class="text-titles">Admin Menu</p>
+            <div class="admin-menu-current">Admin Dashboard</div>
+            <div class="admin-menu-buttons">Alumni Directory</div>
+            <div class="admin-menu-buttons">Announcement Editor</div>
+            <div class="admin-menu-buttons">Event Organizer</div>
+            <div class="admin-menu-buttons">Perks and Discounts</div>
+            <div class="admin-menu-buttons">NU Alumni Tracer</div>
+            <div class="admin-menu-buttons">Messages</div>
+            <div class="admin-menu-buttons">Settings</div>
+
+            <div class="admin-menu-signout">Sign Out</div>
         </div>
     </div>
 
